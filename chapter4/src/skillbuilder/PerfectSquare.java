e30e1e48ff564eaffa2dc562fbf00fe8ad192b9e
49273012 <49273012@L815WD220011.edu.cbe.ab.ca>
2025-10-29 10:58:07 -0600

    10:57am

--- chapter4/src/skillbuilder/PerfectSquare.java
+++ chapter4/src/skillbuilder/PerfectSquare.java
@@ -8,7 +8,21 @@ public class PerfectSquare {
 	{
 		Scanner input = new Scanner(System.in);
 		
-	
+		System.out.print("Please enter a integer: ");
+		int number = input.nextInt();
+		
+		double squareRoot = Math.sqrt(number);
+		
+		int intRoot = (int) squareRoot;
+		
+		int square = intRoot * intRoot;
+		
+		if (square == number) {
+            System.out.println(number + " is a perfect square.");
+        } else {
+            System.out.println(number + " is not a perfect square.");
+        }
+
 		
 	}
 
