21057709396f1e250ad83f5488a30bd78551b2a8
49273012 <49273012@L815WD220011.edu.cbe.ab.ca>
2025-10-30 09:36:34 -0600

    9:36

--- chapter4/src/mastery/CarRecall.java
+++ chapter4/src/mastery/CarRecall.java
@@ -8,8 +8,27 @@ public class CarRecall {
 	{
 		Scanner input = new Scanner(System.in);
 		
+		System.out.print("Welcome to CarRecall, please enter your car model number: ");
+		int car_model = input.nextInt();
 	
+		int[] defectiveModel = {119, 179, 189, 195, 221, 780};
 		
-	}
+		 boolean isDefective = false;
 
+	        for (int defective : defectiveModel) {
+	            if (car_model == defective) {
+	                isDefective = true;
+	                break;
+	            }
+	        }
+                    
+        if (isDefective) 
+        {
+        	System.out.println("Your car is defective, please bring your car to a shop.");
+        } else {
+            System.out.println("Your car is not defective, thanks for stopping by!");
+        }
+    }
 }
+
+
