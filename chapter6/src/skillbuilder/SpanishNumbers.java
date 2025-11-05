38c666f197d3b15ae22ba884beea3b7e7738c557
49273012 <49273012@L815WD220011.edu.cbe.ab.ca>
2025-11-05 11:05:56 -0700

    11:05

--- chapter6/src/skillbuilder/SpanishNumbers.java
+++ chapter6/src/skillbuilder/SpanishNumbers.java
@@ -1,11 +1,41 @@
 package skillbuilder;
+
 import java.util.Scanner;
 
 public class SpanishNumbers {
+	
+		public static void displayInSpanish(int number) {
+	        String spanishWord;
+		 
+	        switch (number) {
+            case 1: spanishWord = "uno"; break;
+            case 2: spanishWord = "dos"; break;
+            case 3: spanishWord = "tres"; break;
+            case 4: spanishWord = "cuatro"; break;
+            case 5: spanishWord = "cinco"; break;
+            case 6: spanishWord = "seis"; break;
+            case 7: spanishWord = "siete"; break;
+            case 8: spanishWord = "ocho"; break;
+            case 9: spanishWord = "nueve"; break;
+            case 10: spanishWord = "diez"; break;
+            default:
+                spanishWord = "Número fuera de rango (1-10)";
+        }
+	        
+	        System.out.println(number + " = " + spanishWord);
+	    }
 
-	
-	
-	
+	    public static void main(String[] args) {
+	        Scanner input = new Scanner(System.in);
+
+	        System.out.println("Enter 10 numbers (from 1 to 10):");
+
+	        
+	        for (int i = 1; i <= 10; i++) {
+	            System.out.print("Enter number " + i + ": ");
+	            int number = input.nextInt();
+	            displayInSpanish(number); 
+	        }
 		
-		
+	  }
 	}
--- chapter6/src/skillbuilder/TimeConverter.java
+++ chapter6/src/skillbuilder/TimeConverter.java
@@ -3,9 +3,40 @@ package skillbuilder;
 import java.util.Scanner;
 
 public class TimeConverter {
+   public static void main(String[] args) {
+	 Scanner input = new Scanner(System.in);
+	 
+	 System.out.println("Time Converter Menu:");
+     System.out.println("1. Hours to Minutes");
+     System.out.println("2. Days to Hours");
+     System.out.println("3. Minutes to Hours");
+     System.out.println("4. Hours to Days");
+     System.out.print("Enter your choice (1-4): ");
+     
+     int choice = input.nextInt();
+     
+     System.out.print("Enter the time value to convert: ");
+     double time = input.nextDouble();
 
-
-	
-		
-		
-	}
+     double convertedTime = 0; 
+     String resultMessage = "";
+     
+     if (choice == 1) {
+         convertedTime = time * 60;  
+         resultMessage = time + " hour(s) = " + convertedTime + " minute(s)";
+     } else if (choice == 2) {
+         convertedTime = time * 24;  
+         resultMessage = time + " day(s) = " + convertedTime + " hour(s)";
+     } else if (choice == 3) {
+         convertedTime = time / 60;  
+         resultMessage = time + " minute(s) = " + convertedTime + " hour(s)";
+     } else if (choice == 4) {
+         convertedTime = time / 24;  
+         resultMessage = time + " hour(s) = " + convertedTime + " day(s)";
+     } else {
+         resultMessage = "Invalid choice!";
+     }
+     
+     System.out.println(resultMessage);
+ }
+}
